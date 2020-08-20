798f094a85ff15fa9f5fba2efb3f5787a385ea2f
Owen Duffy <duffyow@tcd.ie>
2020-08-20 16:27:51 +0100

    Added card number to output and moved rules to separate file

--- Kings.py
+++ Kings.py
@@ -1,20 +1,23 @@
 import random
+import ast
 
 # change the strings here to suit your ruleset
-rules = { 1: "Waterfall",
-            2: "You",
-            3: "Me",
-            4: "Floor",
-            5: "Guys",
-            6: "Chicks",
-            7: "Heaven",
-            8: "Mate",
-            9: "Rhyme",
-            10: "Categories",
-            11: "Never Have I Ever",
-            12: "Quizmaster",
-            13: "King"
-            }
+with open("rules.txt", "r") as data:
+    rules = ast.literal_eval(data.read())
+# rules = { 1: "Waterfall",
+#             2: "You",
+#             3: "Me",
+#             4: "Floor",
+#             5: "Guys",
+#             6: "Chicks",
+#             7: "Heaven",
+#             8: "Mate",
+#             9: "Rhyme",
+#             10: "Categories",
+#             11: "Never Have I Ever",
+#             12: "Quizmaster",
+#             13: "King"
+#             }
 suit = list(range(1, 14))
 deck = suit + suit + suit + suit
 
@@ -28,4 +31,4 @@ while(len(deck) > 0):
         if(kingsCount == 4):
             print("KINGS CUP!!!")
             break
-    print(rules.get(card))
+    print("" + str(card) + " : " + rules.get(card))
